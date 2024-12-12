b8ca16b1e3a3174487f92d292385c672c9b84758
Dmitriy Derepko <xepozz@list.ru>
2024-12-12 20:07:05 +0300

    Initial commit

---
+++ config/common/params.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use App\ViewInjection\CommonViewInjection;
+use App\ViewInjection\LayoutViewInjection;
+use App\ViewInjection\TranslatorViewInjection;
+use Yiisoft\Assets\AssetManager;
+use Yiisoft\Definitions\Reference;
+use Yiisoft\Router\CurrentRoute;
+use Yiisoft\Router\UrlGeneratorInterface;
+use Yiisoft\Translator\TranslatorInterface;
+use Yiisoft\Yii\View\Renderer\CsrfViewInjection;
+
+return [
+    'app' => [
+        'charset' => 'UTF-8',
+        'locale' => 'en',
+        'name' => 'My Project',
+    ],
+
+    'yiisoft/aliases' => [
+        'aliases' => require __DIR__ . '/aliases.php',
+    ],
+
+    'yiisoft/translator' => [
+        'locale' => 'en',
+        'fallbackLocale' => 'en',
+        'defaultCategory' => 'app',
+    ],
+
+    'yiisoft/view' => [
+        'basePath' => '@views',
+        'parameters' => [
+            'assetManager' => Reference::to(AssetManager::class),
+            'urlGenerator' => Reference::to(UrlGeneratorInterface::class),
+            'currentRoute' => Reference::to(CurrentRoute::class),
+            'translator' => Reference::to(TranslatorInterface::class),
+        ],
+    ],
+
+    'yiisoft/yii-view-renderer' => [
+        'viewPath' => '@views',
+        'layout' => '@layout/main.php',
+        'injections' => [
+            Reference::to(CommonViewInjection::class),
+            Reference::to(CsrfViewInjection::class),
+            Reference::to(LayoutViewInjection::class),
+            Reference::to(TranslatorViewInjection::class),
+        ],
+    ],
+];
